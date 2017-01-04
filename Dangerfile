698236cc89859ea9f57e45dd07b835dc23cc7035
Anton Domashnev <antondomashnev@gmail.com>
2017-01-04 22:13:50 +0100

    Initial commit;

---
+++ Dangerfile
@@ -0,0 +1,5 @@
+warn("I'm warning yo")
+
+fail("I can fail your PR in a moment")
+
+message("100 message")
---
+++ danger-playground/danger-playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  danger-playground
+//
+//  Created by Anton Domashnev on 04/01/2017.
+//  Copyright © 2017 Anton Domashnev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
